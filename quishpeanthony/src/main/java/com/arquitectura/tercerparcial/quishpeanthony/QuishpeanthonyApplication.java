3fe84bdf48b9bc8e85a83ff2cfd6132b7db6de68
Anthony Quishpe <aequishpe2@espe.edu.ec>
2025-03-07 17:48:42 -0500

    exmaen3p

---
+++ quishpeanthony/src/main/java/com/arquitectura/tercerparcial/quishpeanthony/QuishpeanthonyApplication.java
@@ -0,0 +1,13 @@
+package com.arquitectura.tercerparcial.quishpeanthony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuishpeanthonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuishpeanthonyApplication.class, args);
+	}
+
+}
---
+++ quishpeanthony/src/test/java/com/arquitectura/tercerparcial/quishpeanthony/QuishpeanthonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitectura.tercerparcial.quishpeanthony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuishpeanthonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
